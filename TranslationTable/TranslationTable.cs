9e13d1a62b434790c5894d6e9bbc0759cb841164
Florian Fida <f+neno@fida.biz>
2015-10-09 22:03:44 +0200

    init

---
+++ TranslationTable/TranslationTable.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+
+namespace TranslationTable
+{
+
+    public class ReadOnlyException : System.MemberAccessException { }
+
+    public interface ISimpleStringTranslationTable{
+        string this[string key] { get; set; }
+        StringDictionary ToStringDictionary();
+        ISimpleStringTranslationTable Add(string key, string value);
+    }
+
+    public class ReadonlySimpleStringTranslationTable : SimpleStringTranslationTable, ISimpleStringTranslationTable {
+        public ReadonlySimpleStringTranslationTable(ISimpleStringTranslationTable initData = null) : base(initData) {
+            seal();
+        }
+
+        public new StringDictionary ToStringDictionary() {
+            var dict = base.ToStringDictionary();
+            var rval = new StringDictionary();
+            foreach(DictionaryEntry entry in base.ToStringDictionary()) {
+                rval.Add((string)entry.Key, (string)entry.Value);
+            }
+            return rval;
+        }
+    }
+
+    public class SimpleStringTranslationTable : ISimpleStringTranslationTable
+    {
+        private StringDictionary stringDict;
+        private bool isSealed = false;
+        private readonly object mutex = new object();
+        private void init(StringDictionary initData) {
+            lock(mutex) {
+                stringDict = initData ?? new StringDictionary();
+            }
+        }
+
+        protected internal void seal() {
+            lock(mutex) { isSealed = true; }
+        }
+
+        public SimpleStringTranslationTable(StringDictionary initData) {
+            init(initData);
+        }
+        public SimpleStringTranslationTable(ISimpleStringTranslationTable initData = null) {
+            init(initData == null ? null : initData.ToStringDictionary());
+        }
+
+        public bool KeyAvailable(string key) {
+            return null == this[key,null];
+        }
+        public ISimpleStringTranslationTable Add(string key, string value) {
+            this[key]=value;
+            return this;
+        }
+
+
+        public StringDictionary ToStringDictionary() {
+            lock(mutex) { return stringDict; }
+        }
+        public string this[string key] {
+            get { return this[key, key]; }
+            set { this[key, null]=value; }
+        }
+        public string this[string key, string defaultValue]{
+            get{
+                lock(mutex) { return stringDict[key] ?? defaultValue; }
+            }
+            set { lock(mutex) {
+                    if(isSealed) throw new ReadOnlyException();
+                    if(stringDict[key] != null) stringDict.Remove(key);
+                    if(value != null) { stringDict.Add(key, value); } 
+            }}
+        }
+    }
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TranslationTable;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Linq;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            var tt = new SimpleStringTranslationTable();
+            tt["key"]="value";
+            Assert.IsTrue(tt["key"]=="value");
+
+            tt["key"]="xxx";
+            Assert.IsTrue(tt["key"]=="xxx");
+
+            tt["key"]=null;
+            Assert.IsTrue(tt["key"]=="key");
+        }
+
+
+        [TestMethod, ExpectedException(typeof(ReadOnlyException))]
+        public void TestMethod2() {
+            var tt = new SimpleStringTranslationTable();
+            tt["key"]="value";
+            var ro = new ReadonlySimpleStringTranslationTable(tt);
+            Assert.IsTrue("value"==ro["key"]);
+            Assert.IsTrue("blah"==ro["blah"]);
+            tt["key"]="xyz";
+            Assert.IsTrue(ro["key"]=="xyz");
+            ro["x"]="y"; /* throws */
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Test3() {
+            var sd = new StringDictionary();
+            var tt = new SimpleStringTranslationTable(sd);
+            var ro = new ReadonlySimpleStringTranslationTable(tt);
+            sd.Add("key", "value");
+            tt
+                .Add("key2", "value2")
+                .Add("key3", "value3")
+            ;
+            Assert.IsTrue(sd["key3"]=="value3");
+            Assert.IsTrue(ro["key3"]=="value3");
+            Assert.IsTrue(tt["key3"]=="value3");
+
+            Assert.IsTrue(sd["key"]=="value");
+            Assert.IsTrue(ro["key"]=="value");
+            Assert.IsTrue(tt["key"]=="value");
+
+            var rod = ro.ToStringDictionary();
+            var rox = new SimpleStringTranslationTable(rod);
+            rod.Add("blah", "blx");
+            Assert.IsTrue(ro["blah", null]==null);
+            Assert.IsTrue(rox["blah", null]=="blx");
+
+        }
+    }
+}
