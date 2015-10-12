8d9d02ba3551ab09078d42379a1a4844203f5870
Florian Fida <f+neno@fida.biz>
2015-10-12 21:29:03 +0200

    localdb

--- UnitTestProject2/localdbsetup.bat
+++ UnitTestProject2/localdbsetup.bat
@@ -1,4 +1,7 @@
+set
+SqlLocalDB.exe p MSSQLLocalDB -i
+SqlLocalDB.exe h MSSQLLocalDB MSSQLLocalDB
 SqlLocalDB.exe s MSSQLLocalDB
 SqlLocalDB.exe i MSSQLLocalDB
-SQLCMD.EXE -S "(localdb)\MSSQLLocalDB" -Q "CREATE DATABASE TranslationTable;"
+SQLCMD.EXE -S "(localdb)\.\MSSQLLocalDB" -Q "CREATE DATABASE TranslationTable;"
 EXIT /B 0
