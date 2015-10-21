41a2900e3cd020b8626c3d79a40bf66bfc8b5432
Florian Fida <f+neno@fida.biz>
2015-10-21 21:51:49 +0200

    -

---
+++ xunit/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xunit
+{
+    public class Class1
+    {
+        [Fact]
+        public void xunit_test1(){
+            Assert.Throws(typeof(Exception), () => {
+                throw new Exception();
+            });
+            Assert.True(true);
+        }
+    }
+}
